379e5bc169e87539f16f99086dd2f9cc421a12ff
manojd <manoj.ducher@gmail.com>
2018-01-23 15:53:02 +0100

    added new posts in blog.

--- articles/models.py
+++ articles/models.py
@@ -3,13 +3,12 @@ from django.db import models
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
     body = models.TextField()
     author = models.CharField(max_length = 20)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
     # add thumbnail
-    
+
     # function to show title of articles
     def __str__(self):
         return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Articles
 
 # Create your views here.
 
 def blog(request):
-    return render(request, 'articles/blog.html')
+    blog = Articles.objects.all().order_by('-created_at')
+    context = {'articles': blog}
+    return render(request, 'articles/ListArticles.html', context)
