c39849028fd35ea54f42cc15d68fa8c952223286
manojd <manoj.ducher@gmail.com>
2018-01-23 11:26:55 +0100

    added model for article creation

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url('r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
 ]
